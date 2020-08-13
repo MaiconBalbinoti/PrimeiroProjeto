dc20d29cd6db228c2716a1a1f9dcd9e59b0c2e6d
Maicon <mbamoacir@hotmail.com>
2020-08-13 20:56:19 -0300

    terceira

--- src/excecao.java
+++ src/excecao.java
@@ -1,3 +1,7 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +14,24 @@
  */
 public class excecao {
     public static void main(String[] args) {
-        System.out.println("Consegui");
-        System.out.println("Segundo commit");
+ {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vetor = sc.nextLine().split(" ");
+            int posicao = sc.nextInt();
+            System.out.println(vetor[posicao]);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Não existe indice com letras");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Indice inexistente");
+        }
+
+        System.out.println("Fim do prorama");
+
+    
     }
+}
 }
