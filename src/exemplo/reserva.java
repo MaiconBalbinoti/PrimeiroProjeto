64da79bdb5a618d6c4ea9a25d089978b998692c6
Maicon <mbamoacir@hotmail.com>
2020-08-17 19:32:25 -0300

    Aula-17.08

---
+++ src/exemplo/reserva.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo;
+
+
+ import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class reserva {
+
+    private int numeroreserva;
+    private Date checkin;
+    private Date checkout;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+
+    public reserva(int numeroreserva, Date checkin, Date checkout) {
+        this.numeroreserva = numeroreserva;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public int getNumeroreserva() {
+        return numeroreserva;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public void setNumeroreserva(int numeroreserva) {
+        this.numeroreserva = numeroreserva;
+    }
+
+    public long duracao() {
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public String atualizareserva(Date checkin, Date checkout) {
+        Date now = new Date();
+        if (checkin.before(now) || checkout.before(now)) {
+            return "Erro na reserva,"
+                    + " as datas para atualização"
+                    + " precisam ser futuras";
+        } else if (!checkout.after(checkin)) {
+            return "Erro na reserva,"
+                    + " a data de Check-out"
+                    + " precisa ser depois da data de Check-in";
+        }
+        this.checkin = checkin;
+        this.checkout = checkout;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero da reserva=" + numeroreserva + ",Data de checkin="
+                + sdf.format(checkin)
+                + ", Data de checkout=" + sdf.format(checkout)
+                + ", " + duracao() + " noites";
+    }
+
+}
+
